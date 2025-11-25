b66b93d467320a413b7bacfc8dac1d0a9020eb45
Christopher Ekong <christopher.ekong@gmail.com>
2025-11-25 12:20:01 +0100

    fixed instance type namecheap api issues

--- 01-provider.tf
+++ 01-provider.tf
@@ -4,12 +4,7 @@ provider "aws" {
 
 terraform {
   required_providers {
-    /*
-    namecheap = {
-      source = "namecheap/namecheap"
-      version = "2.2.0"
-    }
-    */
+    
     aws = {
       source = "hashicorp/aws"
       version = "6.0.0-beta2"
@@ -17,15 +12,7 @@ terraform {
   }
 }
 
-/*
-provider "namecheap" {
-  user_name   = var.namecheap_username
-  api_user    = var.namecheap_api_user
-  api_key     = var.namecheap_api_key
-  client_ip   = var.namecheap_client_ip
-  use_sandbox = false
-}
-*/
+
 
 /*provider "aws" {
   region = "eu-north-1"
